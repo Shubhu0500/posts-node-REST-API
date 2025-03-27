7560764039c3d6aed13ac28f53ebcf51f0fdf58a
Shubhu0500 <shubhu0500@gmail.com>
2025-03-27 10:32:35 +0530

    post can be viewed by the user!

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -10,6 +13,7 @@ const MONGODB_uri = process.env.MONGO_URI;
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,6 +26,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(MONGODB_uri)

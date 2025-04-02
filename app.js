34b53089bea35047bb30213512a1d5f68b6895bf
Shubhu0500 <shubhu0500@gmail.com>
2025-04-02 11:05:59 +0530

    implemented websockets

--- app.js
+++ app.js
@@ -65,7 +65,16 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_uri)
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server, {
+      cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+      },
+    });
+    io.on('connection', (socket) => {
+      console.log('Client connected!');
+    });
   })
   .catch((err) => {
     console.log(err);
